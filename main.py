9cccb07226b2ac005cf9b14a3b873997bf45e99e
Abhinandan0557 <abhi.c0557@gmail.com>
2023-02-17 16:38:42 +0530

    model training complete

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
-
+from Insurance.components.model_trainer import ModelTrainer
 from Insurance.components.data_transformation import DataTransformation
+from Insurance.components.model_evaluation import ModelEvaluation
+#from Insurance.components.model_pusher import ModelPusher
 
 '''
 def test_logger_and_expection():
@@ -48,5 +50,28 @@ if __name__=="__main__":
         data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
     
+
+        # Model Trainer 
+        model_trainer_config = config_entity.ModelTrainingConfig(training_pipeline_config = training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+       # Model Evaluation
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config = training_pipeline_config)
+        model_eval = ModelEvaluation(model_eval_config = model_eval_config,
+        data_ingestion_artifact = data_ingestion_artifact,
+        data_transformation_artifact = data_transformation_artifact,
+        model_trainer_artifact = model_trainer_artifact)
+        model_evl_artifact = model_eval.intitate_model_evaluation()
+
     except Exception as e:
         print(e) 
+
+'''
+       # Model Pusher
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+                     data_transformation_artifact= data_transformation_artifact,
+                     model_trainer_artifact= model_trainer_artifact)
+        Model_pusher_artifact = model_pusher.initiate_model_pusher()
+'''    
